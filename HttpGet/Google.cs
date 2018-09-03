0017a655fba4a3104197bb33ba6335413cdf276b
nikithalingala <nikithalingala@gmail.com>
2018-09-03 23:17:50 +0530

    first Commit

---
+++ HttpGet/Google.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpGet
+{
+    class Google
+    {
+        public String GooglePage()
+        {
+            string url = "http://www.google.com";
+            HttpClient httpClient = new HttpClient();
+            Task<string> getTask = httpClient.GetStringAsync(url);
+            string getUrlResult = getTask.Result;
+         
+
+            return getUrlResult;
+        }
+    }
+}
---
+++ HttpGet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Google google = new Google();
+           string result= google.GooglePage();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+       
+    }
+}
